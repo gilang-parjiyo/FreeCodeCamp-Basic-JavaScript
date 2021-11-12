ffd75b0b90c64f36b360e06cc0827f3cc2831383
gilang <gilang.parjiyo@gmail.com>
2021-11-12 09:54:14 +0700

    first-commit

---
+++ Exercises/9-Differences-Between-the-var-and-let-Keywords/var-vs-let.js
@@ -0,0 +1,6 @@
+"use strict";
+let catNameLet = "Oliver";
+let catNameLet = "Meow!"; // will throw error Identifier 'catNameLet' has already been declared
+
+var catNameVar = "Oliver";
+var catNameVar = "Meow!";
